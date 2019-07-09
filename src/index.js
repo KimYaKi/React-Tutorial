d3ef689daaf90f71d0c1bd9f3df13732c08cd4cb
kimjiha <rlawl@DESKTOP-KOHV3DK>
2019-07-09 16:09:21 +0900

    Create React Project

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// Root라는 위치에 App.js를 채우라는 의미
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
